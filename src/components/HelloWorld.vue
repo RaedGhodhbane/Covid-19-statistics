5f4bda8803418f706fbf50499b4906c954341589
RaedGhodhbane <ghodhbane.raed@gmail.com>
2022-01-31 18:31:13 +0100

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import axios, { AxiosResponse } from 'axios'
+
+let a : AxiosResponse<any, any>;
+export default {
+  name: 'HelloWorld',
+  data() {
+
+    return {
+      statistics : a
+    }
+  },
+  mounted() {
+    axios
+    .get('https://coronavirusapifr.herokuapp.com/data/live/france')
+    .then((response) => {
+      this.statistics = response;
+      console.log(this.statistics)
+    })
+  }
+}
+</script>
+
+<template>
+<h1> Date </h1>
+</template>
+
+<style scoped>
+
+</style>
